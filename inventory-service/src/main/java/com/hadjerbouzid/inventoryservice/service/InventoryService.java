5ac57a92e9958aca6b2ca084462d9dadb356cdba
BOUZID Hadjer <bouzidhadjer34gmail.com>
2023-10-31 13:03:52 +0100

    implement Circuit Breaker , Api gateway and discovery server

--- inventory-service/src/main/java/com/hadjerbouzid/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/hadjerbouzid/inventoryservice/service/InventoryService.java
@@ -1,18 +1,35 @@
 package com.hadjerbouzid.inventoryservice.service;
 
+import com.hadjerbouzid.inventoryservice.dto.InventoryResponse;
 import com.hadjerbouzid.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public  boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    @SneakyThrows
+    public  List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+
+                ).toList();
     }
 }
--- order-service/src/main/java/com/hadjerbouzid/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/hadjerbouzid/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.hadjerbouzid.orderservice.service;
 
+import com.hadjerbouzid.orderservice.dto.InventoryResponse;
 import com.hadjerbouzid.orderservice.dto.OrderLineItemsDto;
 import com.hadjerbouzid.orderservice.dto.OrderRequest;
 import com.hadjerbouzid.orderservice.model.Order;
@@ -7,7 +8,9 @@ import com.hadjerbouzid.orderservice.model.OrderLineItems;
 import com.hadjerbouzid.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -15,14 +18,33 @@ import java.util.List;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public  void placeOrder(OrderRequest orderRequest){
+
+    private final WebClient.Builder webClientBuilder;
+    public  String placeOrder(OrderRequest orderRequest){
          Order order = new Order();
          order.setOrderNumber(order.getOrderNumber());
         List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList().stream()
                  .map(this::mapToDto)
                  .toList();
          order.setOrderLineItemsList(orderLineItemsList);
-         orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode).toList();
+        //call Inventory Service , and place order if product is in
+        // stock
+       InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+        boolean allProductsInStock = Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+        if(allProductsInStock) {
+           orderRepository.save(order);
+           return "Order Placed Successfully";
+       } else {
+           throw new IllegalArgumentException("Product is not in stock, please try again later");
+       }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
